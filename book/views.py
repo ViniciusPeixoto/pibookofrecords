a3f72a65cf7698c7ad15e34e265fd8cfbad49625
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2022-12-09 08:48:24 -0300

    Created views with serializers

--- book/views.py
+++ book/views.py
@@ -23,3 +23,119 @@ def user_methods(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+def user_details(request, pk):
+    """
+    Retrieve, update or delete a user
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(user, data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def transaction_methods(request):
+    """
+    List all transactions, or create a new transaction.
+    """
+    if request.method == 'GET':
+        transaction = Transaction.objects.all()
+        serializer = TransactionSerializer(transaction, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TransactionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+def transaction_details(request, pk):
+    """
+    Retrieve, update or delete a transaction
+    """
+    try:
+        transaction = Transaction.objects.get(pk=pk)
+    except Transaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TransactionSerializer(transaction)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TransactionSerializer(transaction, data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        transaction.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def source_methods(request):
+    """
+    List all sources, or create a new source.
+    """
+    if request.method == 'GET':
+        source = Source.objects.all()
+        serializer = SourceSerializer(source, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SourceSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+def source_details(request, pk):
+    """
+    Retrieve, update or delete a source
+    """
+    try:
+        source = Source.objects.get(pk=pk)
+    except Source.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SourceSerializer(source)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SourceSerializer(source, data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        source.delete()
+        return HttpResponse(status=204)
