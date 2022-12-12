4b675505f127d69e0d1faabd62d19ebb488ced22
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2022-12-12 12:00:00 -0300

    endpoints urls

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,9 @@ from book import views
 
 urlpatterns = [
     path('users/', views.user_methods),
+    path('users/<int:pk>', views.user_details),
+    path('transactions/', views.transaction_methods),
+    path('transactions/<int:pk>', views.transaction_details),
+    path('sources/', views.source_methods),
+    path('sources/<int:pk>', views.source_details),
 ]
