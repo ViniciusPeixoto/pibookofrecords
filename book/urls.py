d023221a34e500ed92cbd076b9e9464a990b5e73
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2022-12-16 16:48:09 -0300

    working API using ViewSet

--- book/urls.py
+++ book/urls.py
@@ -1,15 +1,52 @@
 from django.urls import path
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from book import views
+from book.views import UserViewSet, SourceViewSet, TransactionViewSet, api_root
+
+user_list = UserViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+user_details = UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+source_list = SourceViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+source_details = SourceViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+transaction_list = TransactionViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+transaction_details = TransactionViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+transaction_show = TransactionViewSet.as_view({
+    'get': 'show',
+}, renderer_classes=[renderers.StaticHTMLRenderer])
 
 urlpatterns = [
-    path('users/', views.UserMethods.as_view()),
-    path('users/<int:pk>', views.UserDetails.as_view()),
-    path('sources/', views.SourceMethods.as_view()),
-    path('sources/<int:pk>', views.SourceDetails.as_view()),
-    path('transactions/', views.TransactionMethods.as_view()),
-    path('transactions/<int:pk>', views.TransactionDetails.as_view()),
+    path('', api_root),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>', user_details, name='user-details'),
+    path('sources/', source_list, name='source-list'),
+    path('sources/<int:pk>', source_details, name='source-details'),
+    path('transactions/', transaction_list, name='transaction-list'),
+    path('transactions/<int:pk>', transaction_details, name='transaction-details'),
+    path('transactions/<int:pk>/data/', transaction_show, name='transaction-show')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- book/views.py
+++ book/views.py
@@ -1,52 +1,48 @@
-from rest_framework import generics
+from rest_framework import renderers, viewsets
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from book.models import Source, Transaction, User
 from book.serializers import SourceSerializer, TransactionSerializer, UserSerializer
 
 
-class UserMethods(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     """
-    List all users, or create a new user.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetails(generics.RetrieveDestroyAPIView):
-    """
-    Retrieve or delete a user
+    List and retrieves users
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class SourceMethods(generics.ListCreateAPIView):
+class SourceViewSet(viewsets.ModelViewSet):
     """
-    List all sources, or create a new source.
-    """
-    queryset = Source.objects.all()
-    serializer_class = SourceSerializer
-
-
-class SourceDetails(generics.RetrieveDestroyAPIView):
-    """
-    Retrieve, update or delete a source
+    List and retrieves sources
     """
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
 
 
-class TransactionMethods(generics.ListCreateAPIView):
+class TransactionViewSet(viewsets.ModelViewSet):
     """
-    List all transactions, or create a new transaction.
+    List, retrieves and displays transactions
     """
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def show(self, request, *args, **kwargs):
+        transaction = self.get_object()
+        return Response(transaction.transaction_description)
 
-class TransactionDetails(generics.RetrieveDestroyAPIView):
-    """
-    Retrieve, update or delete a transaction
-    """
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        "users": reverse('user-list', request=request, format=format),
+        "sources": reverse('source-list', request=request, format=format),
+        "transactions": reverse('transaction-list', request=request, format=format)
+    })
