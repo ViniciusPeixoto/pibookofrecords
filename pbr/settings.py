7fee3b2ebef0b4cdad066a626266c4e9ff5fd79a
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2023-04-03 11:10:35 -0300

    JSON as default parser

--- pbr/settings.py
+++ pbr/settings.py
@@ -74,7 +74,10 @@ WSGI_APPLICATION = 'pbr.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
 }
 
 
